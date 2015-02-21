45aaf8f29e99b9e44f16361baf9d0f3e63e9f4d6
jeanne <jeanne.marie.jones@gmail.com>
2015-02-21 13:52:51 -0800

    Final version with some error handling

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are used with matricies in order to save the inverse with
+## the matrix once it is calculated.  This saves computation time for large 
+## matricies.
 
-## Write a short comment describing this function
+## function makeCacheMatrix creates a cached data structure to hold the
+## matrix and its inverse.  Once the inverse is calculated, it can be stored
+## and retrieved from cache.  It also checks if the matrix is numeric
+## and square.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    if (!is.numeric(x)) {
+        message("The input matrix must be numeric")
+        invisible(x)
+        return
+    }
+    if (nrow(x) != ncol(x)) {
+        message("The number of rows and columns must be the same")
+        invisible(x)
+        return
+    }
+    # Create the caching data structure
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## function cacheSolve uses the makeCacheMatrix data structure to check 
+## first if an inverse has already been calculated. If so, it returns
+## the stored version. If not, it calculates the inverse and puts it
+## into the data structure.  It also catches any errors on the solve.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    
+    inv <- tryCatch({solve(data, ...)},
+        error=function(cond) {
+        message("Unable to calculate an inverse for this matrix")
+        return(NA)})
+    
+    x$setinv(inv)
+    return(inv)
 }
